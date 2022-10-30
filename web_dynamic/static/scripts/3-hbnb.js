5467d662e26ce74898bfdf7fcdf1fcdbe550eda9
SNderi <nderisharonw@gmail.com>
2022-10-30 17:39:31 +0300

    Update 3-hbnb.js

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -17,7 +17,7 @@ $(document).ready(() => {
 function apiStatus() {
     const url = `http://0.0.0.0:5001/api/v1/status/`;
     $.get(url, (data, textStatus) => {
-        if (textStatus == 'success' && data.status == 'OK') {
+        if (textStatus === 'success' && data.status === 'OK') {
            $('#api_status').addClass('available');
 	} else {
            $('#api_status').removeClass('available');
@@ -33,20 +33,20 @@ function fetchPlaces() {
 	headers: { 'Content-Type': 'application/json'},
 	data: JSON.stringify({}),
 	success: function (response) {
-           for (const res of response) {
+           for (const place of response) {
               const article = [
 		 '<article>',
                      '<div class="title_box">',
-		         `<h2>${r.name}</h2>`,
-		         `<div class="price_by_night">$${r.price_by_night}</div>`,
+		         `<h2>${place.name}</h2>`,
+		         `<div class="price_by_night">$${place.price_by_night}</div>`,
 		     '</div>'
 		     '<div class="information">',
-		         `<div class="max_guest">${r.max_guest} Guest(s)</div>`,
-		         `<div class="number_rooms">${r.number_rooms} Bedroom(s)</div>`,
-		         `<div class="number_bathrooms">${r.number_bathrooms} Bathroom(s)</div>`,
+		         `<div class="max_guest">${place.max_guest} Guests</div>`,
+		         `<div class="number_rooms">${place.number_rooms} Bedroom(s)</div>`,
+		         `<div class="number_bathrooms">${place.number_bathrooms} Bathroom(s)</div>`,
 		     '</div>'
 		     '<div class="description">',
-		         `${r.description}`,
+		         `${place.description}`,
 		     '</div>'
 		 '</article>'];
                  $('SECTION.places').append(article.join(''));
