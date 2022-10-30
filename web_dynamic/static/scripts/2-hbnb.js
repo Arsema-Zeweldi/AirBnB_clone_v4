e5d3b07d142b58c8e6296337cb93a922e9cc4524
SNderi <nderisharonw@gmail.com>
2022-10-30 17:43:37 +0300

    Add empty dictionary check

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -6,7 +6,11 @@ $(document).ready(() => {
     } else if (!$(this).prop('checked')) {
       delete amenityChoices[$(this).attr('data-id')];
     }
-    $('div.amenities h4').text(Object.values(amenityChoices).join(', '));
+    if (Object.keys(amenityChoices).length === 0) {
+      $('div.amenities h4').html('&nbsp');
+    } else {
+      $('div.amenities h4').text(Object.values(amenityChoices).join(', '));
+    }
   });
 });
 
