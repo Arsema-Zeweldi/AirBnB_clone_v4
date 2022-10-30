dfddcbb343c8690c5452ccaa9677bc671c843cde
SNderi <nderisharonw@gmail.com>
2022-10-30 17:29:21 +0300

    Update 2-hbnb.js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,14 +1,14 @@
 $(document).ready(() => {
   const amenityChoices = {}
   $('input[type="checkbox"]').change(() => {
-    if(this.checked) {
-      amenityChoices[$(this).attr('data-name')] = $(this).attr('data-id');
+    if ($(this).prop('checked')) {
+      amenityChoices[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else if (!$(this).prop('checked')) {
+      delete amenityChoices[$(this).attr('data-id')];
     }
-    else {
-      delete amenityChoices[$(this).attr('data-name')]
-    }
-    $('.amenities h4').text(Object.keys(amenityChoices).sort().join(', '));
+    $('div.amenities h4').text(Object.values(amenityChoices).join(', '));
   });
+});
 
     apiStatus();
 });
